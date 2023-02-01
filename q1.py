2bc31ae589f9107f25a83f040aafc58765876799
HarryAndrus <iamjoshmay@gmail.com>
2023-02-01 13:41:27 -0800

    Chapter2 Lab1

--- q1.py
+++ q1.py
@@ -1,1 +1,12 @@
-print('Hello World')
+# Joshua May
+# 2/01/23
+
+original_price = int(input('What is the regular price? '))
+rate = int(input('What is the percentage discount of the item? ')) / 100
+discount_amount = rate * original_price
+final_amount = original_price - discount_amount
+print('Regular Price: ','$', original_price)
+print('Discount Amount: ','$', int(discount_amount))
+print('The Final Price: ','$', int(final_amount))
+
+ 
